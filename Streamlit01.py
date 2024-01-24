f7e3ac9e682c9a0aa55268b09f76b65d0e3e63ad
nadarktong <neangez@gmail.com>
2024-01-24 11:27:03 +0700

    Update Streamlit01.py

--- Streamlit01.py
+++ Streamlit01.py
@@ -26,13 +26,31 @@ else:
 
 if(st.button("แสดงกราฟแท่ง")):
     chart_data = pd.DataFrame(
-    {
-        "ประเภทดอกไม้": df['variety'],
-        "ความกว้าง": df['sepal.width'],
-        "ความยาว": df['sepal.length']    
-        }
-    )
+{
+    "ประเภทดอกไม้": df['variety'],
+    "ความกว้าง": df['sepal.width'],
+    "ความยาว": df['sepal.length']    
+    }
+)
     st.bar_chart(chart_data, x="ประเภทดอกไม้", y=["ความกว้าง","ความยาว"], color=["#FF0000", "#0000FF"])
     st.button("ไม่แสดงกราฟแท่ง")
 else:
     st.button("ไม่แสดงกราฟแท่ง")
+
+import matplotlib.pyplot as plt
+
+# Pie 
+labels = 'sepal.width', 'sepal.length', 'petal.width', 'petal.length'
+x1=df['sepal.width'].mean()
+x2=df['sepal.length'].mean()
+x3=df['petal.width'].mean()
+x4=df['petal.length'].mean()
+sizes = [x1, x2, x3, x4]
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+#ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
